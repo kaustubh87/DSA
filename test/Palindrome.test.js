02406b5dc765714278335b070e87268f517ff1a3
kaustubh87 <kaustubh87@gmail.com>
2021-01-04 19:09:16 -0500

    Updated test

---
+++ test/Palindrome.test.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var palindrome = require("../Palindrome");
+describe('Check if the string is Palindrome', function () {
+    
+        it('should return true when the string is palindrome', function () {
+            palindrome.
+            assert.equal([1, 2, 3].indexOf(4), -1);
+        });
+
+});
