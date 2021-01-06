60cd9a1ed1497f89eef640db07210101ceda8ab6
kaustubh87 <kaustubh87@gmail.com>
2021-01-05 23:03:18 -0500

    Debug code

--- test/Palindrome.test.js
+++ test/Palindrome.test.js
@@ -9,14 +9,14 @@ describe('Check if the string is Palindrome', function () {
         it('should return true when the string is palindrome', function () {
             let response = palindrome.isPalindrome("nitin");
             expect(response).to.equal(true);
-            //console.log(response);
+            
             
         });
 
         it('should return false when the string is palindrome', function () {
             let response = palindrome.isPalindrome("Nipni");
             expect(response).to.equal(false);
-           // console.log(response);
+           
             
         });
 
